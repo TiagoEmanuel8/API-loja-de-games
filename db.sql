8a90a5e1f8cfe2e748d2aa5fe4d2ce1744b89d1f
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2021-12-10 18:13:39 -0300

    Removendo db extra

--- db.sql
+++ db.sql
@@ -4,20 +4,20 @@ CREATE DATABASE IF NOT EXISTS `app-storeGame`;
 USE `app-storeGame`;
 
 CREATE TABLE IF NOT EXISTS users (
-	id INT NOT NULL AUTO_INCREMENT,
-  name VARCHAR(100) NOT NULL,
+  id INT NOT NULL AUTO_INCREMENT,
+  name TEXT(100) NOT NULL,
   email VARCHAR(100) NOT NULL,
-  password VARCHAR(64) NOT NULL,
-  cpf VARCHAR(13) NOT NULL,
-  mobile_number VARCHAR(15),
+  password VARCHAR(160) NOT NULL,
+  cpf INT(11) NOT NULL,
+  mobile_number INT(15),
   address VARCHAR(100) NOT NULL,
-  address_number INT(6),
-  district VARCHAR(50) NOT NULL,
-  city VARCHAR(50) NOT NULL,
-  state TEXT(4),
-  country TEXT(50) NOT NULL,
+  address_number INT(100),
+  district TEXT(100) NOT NULL,
+  city TEXT(50) NOT NULL,
+  state TEXT(20) NOT NULL,
+  country TEXT(3) NOT NULL,
   cep INT(8) NOT NULL,
-  role VARCHAR(20) NOT NULL,
+  role TEXT(20) NOT NULL,
   PRIMARY KEY (id),
   UNIQUE KEY `email_un` (email)
 );
@@ -38,7 +38,7 @@ CREATE TABLE IF NOT EXISTS products (
   id INT NOT NULL AUTO_INCREMENT,
   name VARCHAR(100) NOT NULL,
   type VARCHAR(30) NOT NULL,
-  price DECIMAL(9,2) NOT NULL,
+  price DECIMAL(4,2) NOT NULL,
   url_image VARCHAR(200) NOT NULL DEFAULT '',
   PRIMARY KEY(id),
   UNIQUE KEY `name` (name)
@@ -52,29 +52,3 @@ CREATE TABLE IF NOT EXISTS sales_products (
   FOREIGN KEY(sale_id) REFERENCES sales(id),
   FOREIGN KEY(product_id) REFERENCES products(id)
 );
-
-INSERT INTO users (id, name, email, password, cpf, mobile_number, address, address_number, district, city, state, country, cep, role) VALUES
-	(1,
-    'Filipe Bernardo Eduardo Costa',
-    'filipebernardoeduardocosta@gmail.com',
-    '$2a$10$u0FzQuq1dId4Sd2L0B03FOwR7YyPi7FB0mJPsn1ELhEcS7o8l81JC',
-    46360452464,
-    8326447157,
-    'Rua Cruzeiro do Sul',
-    24,
-    'Calafate',
-    'Rio Branco',
-    'AC',
-    'BR',
-    69914374,
-    'administrator'
-    );
-    
-INSERT INTO sales (id, user_id, seller_id, total_price, sale_date, status_sale) VALUES
- 	(2, 3, 2, 169.8, "2021/12/08", "pagamento concluido");
-    
-INSERT INTO products (id, name, type, price, url_image) VALUES
-	(1, 'Battlefield 2042', 'Xbox One | X|S', 210.5, 'http://localhost:3001/images/bf2042_xbox.jpg');
-
-INSERT INTO sales_products (sale_id, product_id, quantity) VALUES
-	(1, 2, 6);
