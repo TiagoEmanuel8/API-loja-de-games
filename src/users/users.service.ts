3ed55faaa0fc49f99a5f24479f19a8d134b7118d
TiagoEmanuel8 <tiago.emanuel.n@gail.com>
2023-04-12 22:00:30 -0300

    feat: criando endpoint para listar clientes

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,19 +1,24 @@
+import { PrismaService } from 'src/database/PrismaService';
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
+  constructor(private prisma: PrismaService) {}
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
   findAll() {
-    return `This action returns all users`;
+    return this.prisma.users.findMany();
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} user`;
+    return this.prisma.users.findUnique({
+      where: { id: Number(id) },
+    });
   }
 
   update(id: number, updateUserDto: UpdateUserDto) {
