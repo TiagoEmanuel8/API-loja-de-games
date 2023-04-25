f36931350e43e0ce5ba57421a1c9d2d0c2464802
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-04-25 20:33:21 -0300

    feat: configurando seeders vendas por produtos

---
+++ src/database/seeders/20230425233207-salesproducts.js
@@ -0,0 +1,35 @@
+"use strict";
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert(
+      "salesProducts",
+      [
+        {
+          sale_id: 1,
+          product_id: 2,
+          quantity: 6,
+        },
+        {
+          sale_id: 2,
+          product_id: 2,
+          quantity: 9,
+        },
+        {
+          sale_id: 3,
+          product_id: 4,
+          quantity: 12,
+        },
+      ],
+      {
+        tableName: "salesProducts",
+        timestamps: false,
+        underscored: true,
+      }
+    );
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete("salesProducts", null, {});
+  },
+};
