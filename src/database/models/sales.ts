861850b4bf29fc8123abbcf9b5c97e9bcfb587d1
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-04-25 20:10:30 -0300

    feat: configurando relacionamentos

--- src/database/models/sales.ts
+++ src/database/models/sales.ts
@@ -1,7 +1,8 @@
 import { DataTypes, Model } from 'sequelize';
 import db from '.';
+import Users from './users';
 
-class Products extends Model {
+class Sales extends Model {
   declare id: number;
   declare userId: string;
   declare sellerId: string;
@@ -10,7 +11,7 @@ class Products extends Model {
   declare statusSale: string;
 }
 
-Products.init({
+Sales.init({
   id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
   userId: DataTypes.STRING,
   sellerId: DataTypes.STRING,
@@ -24,4 +25,36 @@ Products.init({
   timestamps: false,
 });
 
-export default Products;
+// Sale.associate = (models) => {
+//   Sale.belongsTo(models.User, { foreignKey: 'userId', as: 'user_id' });
+  // Sale.belongsTo(models.User,
+  //   { foreignKey: 'userId', as: 'users' },
+  //   { foreignKey: 'sellerId', as: 'users' }
+  // );
+// };
+
+//   User.hasMany(models.Sale, { foreignKey: 'id', as: 'user_id' });
+Users.hasMany(Sales, {
+  foreignKey: 'id',
+  as: 'user_id'
+})
+
+Sales.belongsTo(Users, {
+  foreignKey: 'userId',
+  as: 'user_id'
+})
+
+// import Clubs from './Club';
+// Matchs.init({})
+
+// Clubs.hasMany(Matchs, {
+//   foreignKey: 'homeTeam',
+//   as: 'homeMatchs',
+// });
+
+// Matchs.belongsTo(Clubs, {
+//   foreignKey: 'homeTeam',
+//   as: 'homeClub',
+// });
+
+export default Sales;
