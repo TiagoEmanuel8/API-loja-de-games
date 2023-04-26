3fa3155ad66964397f55342354ef3473a518e4aa
TiagoEmanuel8 <tiago.emanuel.n@gmail.com>
2023-04-26 14:35:30 -0300

    feat: definindo testes para rota usuarios

--- src/test/users.test.ts
+++ src/test/users.test.ts
@@ -7,25 +7,25 @@ chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('testando o endpoint /products', () => {
-  describe('', async () => {
-    describe('testando o endpoint /products com o metodo getProducts', async () => {
+describe('testando o endpoint /users', () => {
+  describe('testando o endpoint /users com o metodo getUsers', async () => {
+    describe('retorna com sucesso', async () => {
 
       let chaiHttpResponse: Response;
 
-      it('A aplicação deve ter o endpoint GET `/products` para listar produtos', async () => {
+      it('A aplicação deve ter o endpoint GET `/users` para listar usuarios', async () => {
         chaiHttpResponse = await chai
         .request(app)
-        .get('/products')
+        .get('/users')
         .then((res) => {
           expect(res.status).to.be.equal(200);
         }) as Response;
       });
 
-      it('A aplicação deve ter o endpoint GET `/products/:id` para listar um produto', async () => {
+      it('A aplicação deve ter o endpoint GET `/users/:id` para listar um usuario', async () => {
         chaiHttpResponse = await chai
         .request(app)
-        .get('/products')
+        .get('/users')
         .then((res) => {
           expect(res.status).to.be.equal(200);
         }) as Response;
